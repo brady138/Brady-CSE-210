1ddc3c9b5bb3dc19951e1d0f61a1a8903a1d6a61
Brady P <bradypurtill138@gmail.com>
2023-06-10 22:15:10 -0700

    Complete Prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Enter numbers (Type '0' when finished):");
+
+        while (true)
+        {
+            int input = Convert.ToInt32(Console.ReadLine());
+            if (input == 0)
+                break;
+
+            numbers.Add(input);
+        }
+
+        if (numbers.Count > 0)
+        {
+            int sum = numbers.Sum();
+            int max = numbers.Max();
+            double average = numbers.Average();
+
+            Console.WriteLine($"Sum: {sum}");
+            Console.WriteLine($"Highest number: {max}");
+            Console.WriteLine($"Average: {average}");
+        }
+        else
+        {
+            Console.WriteLine("No numbers entered.");
+        }
     }
 }
