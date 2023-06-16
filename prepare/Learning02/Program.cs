64e9b5b979c0241d41c2da1ecd572518eab39941
Brady P <bradypurtill138@gmail.com>
2023-06-15 21:29:53 -0700

    Complete Prep 2

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,57 @@
 using System;
 
-class Program
+public class Job
+{
+    public string _jobTitle;
+    public string _company;
+    public int _start;
+    public int _end;
+
+    public void Display()
+    {
+        Console.WriteLine($"{_jobTitle} ({_company}) {_start}-{_end}");
+    }
+}
+public class Resume
+{
+    public string _name;
+
+    public List<Job> _jobs = new List<Job>();
+
+    public void Display()
+    {
+        Console.WriteLine($"Name: {_name}");
+        Console.WriteLine("Jobs:");
+
+        foreach (Job job in _jobs)
+        {
+            job.Display();
+        }
+    }
+}
+public class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Senior Software Engineer";
+        job1._company = "Microsoft";
+        job1._start = 2003;
+        job1._end = 2022;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Software Engineer";
+        job2._company = "Apple";
+        job2._start = 2001;
+        job2._end = 2003;
+
+        Resume myResume = new Resume();
+        myResume._name = "Allison Rose";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
+
