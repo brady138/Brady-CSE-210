38f5f044ad6403cf521435e55918d672809bb650
Brady P <bradypurtill138@gmail.com>
2023-06-27 12:14:16 -0700

    done

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,45 @@
-using System;
-
-class Program
+public class Fraction
 {
-    static void Main(string[] args)
+    private int top;    
+    private int bottom; 
+
+    public Fraction()
     {
-        Console.WriteLine("Hello Learning03 World!");
+        top = 1;
+        bottom = 1;
+    }
+
+    public Fraction(int numerator)
+    {
+        top = numerator;
+        bottom = 1;
+    }
+
+    public Fraction(int numerator, int denominator)
+    {
+        top = numerator;
+        bottom = denominator;
+    }
+
+    public int Top
+    {
+        get { return top; }
+        set { top = value; }
+    }
+
+    public int Bottom
+    {
+        get { return bottom; }
+        set { bottom = value; }
+    }
+
+    public string GetFractionString()
+    {
+        return top + "/" + bottom;
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double)top / bottom;
     }
 }
